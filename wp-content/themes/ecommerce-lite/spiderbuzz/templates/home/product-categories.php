fa814ac2f92d754a3e7443edb97ee78c9e5d3dd8
SDG Internee 12 <sdg.internee12@int.sybrid.com>
2019-02-28 10:56:58 +0500

    category section

--- wp-content/themes/ecommerce-lite/spiderbuzz/templates/home/product-categories.php
+++ wp-content/themes/ecommerce-lite/spiderbuzz/templates/home/product-categories.php
@@ -12,15 +12,15 @@ function ecommerce_lite_homepage_products_category(){
     //if (!is_array($products_categorys) || count($products_categorys) == 0 || trim($products_categorys[0]) == '') return;
     ?>
     <!-- Category List Section Hear -->
-        <section id="products_category_section" class="catagory-pro">
-            <div class="container">
-                <div class="row">
-                    
-                    <?php
+    <section id="products_category_section" class="catagory-pro">
+        <div class="container">
+            <div class="row">
+
+                <?php
                         //Shift the First Category
-                    if( $products_categoreis_layout == 'random-layout' ){
+                if( $products_categoreis_layout == 'random-layout' ){
                             $products_categorys_first = array_shift( $products_categorys );#category Section
-                    
+
                             //Products Categorys Section
                             $first_category_term = get_term_by( 'id', $products_categorys_first, 'product_cat');
                             
@@ -40,7 +40,7 @@ function ecommerce_lite_homepage_products_category(){
                             if( $image == ''){
                                 $image = esc_url( get_template_directory_uri() . '/assets/images/category-img.jpg' );
                             }
-                        ?>
+                            ?>
                             <div class="col-lg-6 col-md-6 col-sm-12">
                                 <div class="catagory-img">
                                     <figure>
@@ -52,89 +52,94 @@ function ecommerce_lite_homepage_products_category(){
                             <div class="col-lg-6 col-md-6 col-sm-12">
                                 <div class="row">
                                     <?php
-                                        $products_category_count = 1;
-                                        foreach( $products_categorys as $cat_key => $category_id ){ 
-                                        
-                                            if( $products_category_count < 5 ){
+                                    $products_category_count = 1;
+                                    foreach( $products_categorys as $cat_key => $category_id ){ 
+
+                                        if( $products_category_count < 5 ){
                                                 //Products Categorys Section
-                                                $term = get_term_by( 'id', $category_id, 'product_cat');
+                                            $term = get_term_by( 'id', $category_id, 'product_cat');
 
                                                 //term links hear
-                                                if($term == null){
-                                                    $category_id = ecommerce_lite_woo_cat_id_by_slug('uncategorized');
-                                                    
-                                                    $term = get_term_by( 'id', $category_id, 'product_cat');
-                                                    
-                                                }
-                                                
+                                            if($term == null){
+                                                $category_id = ecommerce_lite_woo_cat_id_by_slug('uncategorized');
+
+                                                $term = get_term_by( 'id', $category_id, 'product_cat');
+
+                                            }
+
                                                 //Category Image
-                                                $thumbnail_id = get_woocommerce_term_meta( $category_id, 'thumbnail_id', true );
-                                                $image = wp_get_attachment_url( $thumbnail_id );
+                                            $thumbnail_id = get_woocommerce_term_meta( $category_id, 'thumbnail_id', true );
+                                            $image = wp_get_attachment_url( $thumbnail_id );
 
                                                 //default image set
-                                                if( $image == ''){
-                                                    $image = esc_url( get_template_directory_uri() . '/assets/images/category-img.jpg' );
-                                                } 
+                                            if( $image == ''){
+                                                $image = esc_url( get_template_directory_uri() . '/assets/images/category-img.jpg' );
+                                            } 
                                             ?>
-                                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                                    <div class="catagory-img">
-                                                        <figure>
-                                                            <img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $term->name ); ?>">
-                                                            <a href="<?php echo esc_url( get_category_link( $category_id )); ?>" class="btn text-uppercase"><?php echo esc_html( $term->name ); ?></a>
-                                                        </figure>
-                                                    </div>
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="catagory-img">
+                                                    <figure>
+                                                        <img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $term->name ); ?>">
+
+                                                        <a href="<?php echo esc_url( get_category_link( $category_id )); ?>" class="btn text-uppercase"><?php echo esc_html( $term->name ); ?></a>
+                                                    </figure>
                                                 </div>
+                                            </div>
 
                                             <?php }#End Products Count Section
 
                                             $products_category_count++; 
-                                            } 
+                                        } 
                                         ?>
 
+                                    </div>
                                 </div>
-                            </div>
 
-                    <?php }else{ 
+                            <?php }else{ 
 
                         //Display Two Column And Three Columns 
-                    
-                        foreach( $products_categorys as $cat_key => $category_id ){  
-                            
+
+                                foreach( $products_categorys as $cat_key => $category_id ){  
+
                             //category columns conditions hear
-                            if( $products_categoreis_layout == '2-column' ){
-                                $ecommerce_lite_products_category_class = 'col-lg-6 col-md-6 col-sm-6 col-xs-12';
-                            }elseif( $products_categoreis_layout == '3-column' ){
-                                $ecommerce_lite_products_category_class = 'col-lg-4 col-md-4 col-sm-6 col-xs-12';
-                            }else{
-                                $ecommerce_lite_products_category_class = 'col-lg-3 col-md-3 col-sm-6 col-xs-12';
-                            }
-                            
+                                    if( $products_categoreis_layout == '2-column' ){
+                                        $ecommerce_lite_products_category_class = 'col-lg-6 col-md-6 col-sm-6 col-xs-12';
+                                    }elseif( $products_categoreis_layout == '3-column' ){
+                                        $ecommerce_lite_products_category_class = 'col-lg-4 col-md-4 col-sm-6 col-xs-12';
+                                    }else{
+                                        $ecommerce_lite_products_category_class = 'col-lg-3 col-md-3 col-sm-6 col-xs-12';
+                                    }
+
                             //Products Categorys Section
-                            $term = get_term_by( 'id', $category_id, 'product_cat');
-                                                    
+                                    $term = get_term_by( 'id', $category_id, 'product_cat');
+
                             //Category Image
-                            $thumbnail_id = get_woocommerce_term_meta( $category_id, 'thumbnail_id', true );
-                            $image = wp_get_attachment_url( $thumbnail_id ); 
+                                    $thumbnail_id = get_woocommerce_term_meta( $category_id, 'thumbnail_id', true );
+                                    $image = wp_get_attachment_url( $thumbnail_id ); 
 
                             //default image set
-                            if( $image == ''){
-                                $image = esc_url( get_template_directory_uri() . '/assets/images/category-img.jpg' );
-                            }
-                        ?>
-                        <!-- Start Category Item Loop -->
-                        <div class="<?php echo esc_attr( $ecommerce_lite_products_category_class ); ?>">
-                            <div class="catagory-img">
-                                <figure>
-                                    <img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $term->name ); ?>">
-                                    <a href="<?php echo esc_url( get_category_link( $category_id )); ?>" class="btn text-uppercase"><?php echo esc_html( $term->name ); ?></a>
-                                </figure>
-                            </div>
-                        </div><!-- End Category Item -->
-                    <?php }}#end condtion ?>
+                                    if( $image == ''){
+                                        $image = esc_url( get_template_directory_uri() . '/assets/images/category-img.jpg' );
+                                    }
+                                    ?>
+                                    <!-- Start Category Item Loop -->
+                                    <div class="<?php echo esc_attr( $ecommerce_lite_products_category_class ); ?>">
+                                        <div class="catagory-img">
+                                            <figure>
+                                                <img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $term->name ); ?>">
+                                                <!-- <a href="<?php echo esc_url( get_category_link( $category_id )); ?>" class="mybtn text-uppercase"><?php echo esc_html( $term->name ); ?></a> -->
+                                            </figure>
 
-                </div><!-- End Row -->
-            </div><!-- End Container Class -->
-        </section><!-- End Products Category section --> 
-    <?php
-}
-add_action( 'products-category','ecommerce_lite_homepage_products_category' );
+                                        </div>
+                                        <div class="mydiv">
+                                            <a href="<?php echo esc_url( get_category_link( $category_id )); ?>" class="mybtn text-uppercase"><?php echo esc_html( $term->name ); ?></a>
+                                        </div>
+                                    </div><!-- End Category Item -->
+                                <?php }}#end condtion ?>
+
+                            </div><!-- End Row -->
+                        </div><!-- End Container Class -->
+                    </section><!-- End Products Category section --> 
+                    <?php
+                }
+                add_action( 'products-category','ecommerce_lite_homepage_products_category' );
--- wp-content/themes/ecommerce-lite/spiderbuzz/templates/home/service-box.php
+++ wp-content/themes/ecommerce-lite/spiderbuzz/templates/home/service-box.php
@@ -3,7 +3,7 @@
  * Service Box all Settings Hear
  * 
  */
- function ecommerce_lite_homepage_service_section(){
+function ecommerce_lite_homepage_service_section(){
     //Service Section Default Value
     $defaults = array(
         array(
@@ -31,27 +31,35 @@
     //Service Box Section
     $service_box_items = get_theme_mod( 'homepage_service_box_section', $defaults );
     
-     ?>
-        <section id="frontpage_service_box_section" class="container">
-            <div class="support">
-                <div class="row">
-                    <?php foreach( $service_box_items as $service_item ){ ?>
-                        <div class="col-lg-3 col-md-6 col-sm-6 xs-12 service-box">
-                            <div class="feature-box">
-                                <div class="service-icon">
-                                    <i class="<?php echo esc_attr( $service_item['service_icons'] ); ?> fa-3x"  aria-hidden="true"></i>
-                                </div>
-                                <div class="service-text">
-                                    <div class="ser-title"><?php echo esc_attr( $service_item['service_title'] ); ?></div>
-                                    <div class="ser-subtitle"><?php echo esc_attr( $service_item['service_short_desc'] ); ?></div>   
-                                </div>
+    ?>
+    <section id="frontpage_service_box_section" class="container">
+        <div style="background: ;text-align: center;height: 50px;width: 100%;overflow: hidden;position: relative;margin-top: 50px;">
+
+            <hr style="background:;width: 100%;height: 2px;position: absolute;margin:0px;margin-top: 20px;">
+            
+            <h5 style="font-size: 6px;color:#eee;">x</h5>
+            <h4 style="background: white;display: inline;margin: 0px;z-index: 999;position: relative;margin-top: 20px !important;padding-left: 20px;padding-right: 20px;">OUR SERVICES</h4>
+        </div>
+        <div class="support">
+            <div class="row">
+
+                <?php foreach( $service_box_items as $service_item ){ ?>
+                    <div class="col-lg-3 col-md-6 col-sm-6 xs-12 service-box">
+                        <div class="feature-box">
+                            <div class="service-icon">
+                                <i class="<?php echo esc_attr( $service_item['service_icons'] ); ?> fa-3x"  aria-hidden="true"></i>
+                            </div>
+                            <div class="service-text">
+                                <div class="ser-title"><?php echo esc_attr( $service_item['service_title'] ); ?></div>
+                                <div class="ser-subtitle"><?php echo esc_attr( $service_item['service_short_desc'] ); ?></div>   
                             </div>
                         </div>
-                    <?php } ?>
-                    
-                </div>
+                    </div>
+                <?php } ?>
+
             </div>
-        </section>
+        </div>
+    </section>
 
     <?php
 }
